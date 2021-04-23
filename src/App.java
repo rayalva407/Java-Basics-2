b074bfb5e907739976925d56af237186c9a6b61d
rayalva407 <rayalva407@gmail.com>
2021-04-22 18:15:11 -0600

    adds maxNum method

--- src/App.java
+++ src/App.java
@@ -1,7 +1,21 @@
 public class App {
+    public int maxNum() {
+        int[][] arr = { {1, 4}, {3, 6}, {2, 9} };
+        int max = 0;
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello World");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] > max) {
+                    max = arr[i][j];
+                } 
+            }
+        }
+        return max;
     }
 
+
+    public static void main(String[] args) throws Exception {
+        App app = new App();
+        System.out.println("The maximum value in this 2D array is " + app.maxNum());
+    }
 }
