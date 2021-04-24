f290526f2c99f775782ca15cedf50a8070898f72
rayalva407 <rayalva407@gmail.com>
2021-04-24 13:13:54 -0600

    adds arrIndex method

--- src/App.java
+++ src/App.java
@@ -13,9 +13,25 @@ public class App {
         return max;
     }
 
+    public int[] arrIndex() {
+        int[][] arr = { {1, 4}, {3, 6}, {2, 9} };
+        int[] indexArr = new int[2];
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[i].length; j++) {
+                if (arr[i][j] == maxNum()) {
+                    indexArr[0] = i;
+                    indexArr[1] = j;
+                } 
+            }
+        }
+        return indexArr;
+    }
+
 
     public static void main(String[] args) throws Exception {
         App app = new App();
         System.out.println("The maximum value in this 2D array is " + app.maxNum());
+        System.out.println("The position of the maximum value is [" + app.arrIndex()[0] + ", " + app.arrIndex()[1] + "]");
     }
 }
